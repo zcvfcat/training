61adeb0509d8a3590dd3f97b8747378fa72ad55e
김대현 <zcvfcat@gmail.com>
2020-06-09 14:19:59 +0900

    v

--- algorithm/main.py
+++ algorithm/main.py
@@ -1,26 +1,25 @@
-import sys
- 
-N=int(sys.stdin.readline())
-W=[]
-INF=sys.maxsize
-for _ in range(N):
-    W.append(list(map(int,sys.stdin.readline().split())))
-DP=[[None]*(1<<N) for _ in range(N)]
- 
-def find_path(last,visited):#현재위치
-    if visited == (1<<N)-1:  # 모두 방문햇다면
-        return W[last][0] or INF  # 0으로 가는방법 반환 없으면 INF반환
- 
-    if DP[last][visited] is not None:  # 이미 계산되어잇다면
-        return DP[last][visited]  # 있는값 반환
- 
-    tmp=INF
-    for city in range(N):#모든 도시에서
-        if visited & (1 << city) == 0 and W[last][city] != 0:#아직 방문하지 않았고 cur->i로 가는길이 있다면
-            tmp=min(tmp,
-                    find_path(city,visited | (1<<city)) + W[last][city])
-    DP[last][visited]=tmp
-    return tmp
- 
- 
-print(find_path(0,1<<0))
+n, s, m = map(int, input().split())
+lst = list(map(int, input().split()))
+
+dp1 = [0] * (m + 1)
+dp2 = [0] * (m + 1)
+
+dp1[s] = 1
+
+for v in lst:
+    for i in range(m + 1):
+        if dp1[i]:
+            if i + v <= m:
+                dp2[i + v] = 1
+            if i - v >= 0:
+                dp2[i - v] = 1
+    dp1 = dp2
+    dp2 = [0] * (m + 1)
+
+ans = -1
+for i in range(m, -1, -1):
+    if dp1[i]:
+        ans = i
+        break
+
+print(ans)
